0d89059a3c5105dc10c4a2c9313d832af4106f3b
ariannadepasquale <depasqualearianna@gmail.com>
2022-02-21 16:40:03 +0100

    Add "userName" - "surName" - "favoriteColor"

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+const userName = prompt('Inserisci il tuo nome');
+console.log(userName); 
+
+const surName = prompt('Inserisci il tuo cognome');
+console.log(surName);
+
+const favoriteColor = prompt ('Inserisci il tuo colore preferito');
+console.log (favoriteColor);
+
+
+
+
+
