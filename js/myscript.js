cc673fa29fc10dd2669edf749075a8f858e29183
ariannadepasquale <depasqualearianna@gmail.com>
2022-02-21 16:44:45 +0100

    Add //password (??)

--- js/myscript.js
+++ js/myscript.js
@@ -7,6 +7,9 @@ console.log(surName);
 const favoriteColor = prompt ('Inserisci il tuo colore preferito');
 console.log (favoriteColor);
 
+//PASSWORD
+document.getElementById('password').innerHTML = userName + surName + favoriteColor + '21';
+
 
 
 
